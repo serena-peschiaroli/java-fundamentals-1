4ba8ae15deae925c0b6cc0f30f90c76a3a482091
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-03-27 15:40:13 +0100

    Add: FizzBuzz Class

--- src/org/experis/fizzbuzz/FizzBuzz.java
+++ src/org/experis/fizzbuzz/FizzBuzz.java
@@ -2,19 +2,26 @@ package org.experis.fizzbuzz;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
+            //inizializza result
             String result;
-            String printLine;
 
             if( i % 15 == 0){
+                //se numero divisibile per 3 e 5, Fizzbuzz
                 result = "FizzBuzz";
             } else if (i % 3 == 0) {
+                //se divisibile solo per tre, Fizz
                 result = "Fizz";
             } else if (i % 5 == 0) {
+                //se divisibile solo per 5, Buzz
                 result = "Buzz";
             }else{
+                //altrimenti, stampa numero
+                //valueOf converte diversi valori in una stringa
                 result = String.valueOf(i);
             }
+
+            System.out.println(result);
 
         }
     }
