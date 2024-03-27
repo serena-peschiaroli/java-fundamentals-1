4acf84346ef2033e79d5248fac78a915ce479680
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-03-27 17:34:52 +0100

    Refactor: CalcolaCibi to ask for user input

--- src/org/experis/cibi/CalcolaCibi.java
+++ src/org/experis/cibi/CalcolaCibi.java
@@ -1,18 +1,43 @@
 package org.experis.cibi;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class CalcolaCibi {
     public static void main(String[] args) {
-        // inizializza l'array string con i cibi favoriti
-        String[] favoriteFoods = {"Gelato", "Amatriciana", "Cioccolato Fondente", "Patate fritte", "Occhi di bue", "Pizza", "Sushi","Burritos" };
+
+        //creare elemento scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // chiedere a user numero di cibi che vuole inserire, assicurandosi che sia fra 5 e 10
+        int size = 0;
+        while (size < 5 || size > 10) {
+            System.out.print("how many foods do you want to enter? (min 5 and  max 10): ");
+            size = Integer.parseInt(scanner.nextLine());
+            if (size < 5 || size > 10) {
+                System.out.println("please, enter a number between 5 and 10");
+            }
+        }
+
+        // inizializzare array di cibi favoriti
+        String[] favoriteFoods = new String[size];
+
+        // chiedere all'utente posizione dei cibi
+        for (int i = 0; i < favoriteFoods.length; i++) {
+            System.out.print("enter food at position " + (i + 1) + ": ");
+            favoriteFoods[i] = scanner.nextLine();
+        }
+
+        // Printing the array of favorite foods to confirm the input
+        System.out.println("Your favorites are: " + Arrays.toString(favoriteFoods));
 
         //stampa la lunghezza della classifica
         System.out.println("the length of the raanking is: " + favoriteFoods.length);
 
         //stampa il cibo top
-        System.out.println("My top food is: " + favoriteFoods[0]);
+        System.out.println("your top food is: " + favoriteFoods[0]);
 
         //stampa l'ultimo elemento della lista
-        System.out.println("My last favourite food is: " + favoriteFoods[favoriteFoods.length - 1]);
+        System.out.println("Your last favourite food is: " + favoriteFoods[favoriteFoods.length - 1]);
 
         //elemento in posizione mediana:
 
